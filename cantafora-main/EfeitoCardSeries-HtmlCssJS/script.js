92c0e1caea72862754ca5027d85b942db8327c41
cantass <128430733+cantass@users.noreply.github.com>
2024-02-12 23:43:23 -0300

    Add files via upload

---
+++ cantafora-main/EfeitoCardSeries-HtmlCssJS/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    speed: 500,
+    loop: true,
+    rotate: true,
+    mousewheel: {
+    invert: false,
+  },
+});
+function irParaYouTube() {
+  window.location.href = "https://youtu.be/TJRIZS4Z7Mw";
+}
